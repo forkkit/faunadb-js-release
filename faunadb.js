ad2927a68d70e34c16ddd6491ac27f14a09b206e
Erick Pintor <erickpintor@gmail.com>
2017-03-21 18:39:56 -0300

    release 1.0.1

--- faunadb.js
+++ faunadb.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.faunadb = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * An ES6 compatible promise. This driver depends on the {@link https://github.com/stefanpenner/es6-promise|es6-promise polyfill}.
  * @external Promise
@@ -5467,7 +5467,7 @@ function Select(path, from, _default) {
   arity.between(2, 3, arguments);
   var exprObj = { select: wrap(path), from: wrap(from) };
   if (_default !== undefined) {
-    exprObj.default = wrapValues(_default);
+    exprObj.default = wrap(_default);
   }
   return new Expr(exprObj);
 }
@@ -6045,4 +6045,5 @@ module.exports = {
   Bytes: Bytes
 };
 
-},{"./Expr":18,"./errors":24,"base64-js":2,"util":16}]},{},[1]);
+},{"./Expr":18,"./errors":24,"base64-js":2,"util":16}]},{},[1])(1)
+});
